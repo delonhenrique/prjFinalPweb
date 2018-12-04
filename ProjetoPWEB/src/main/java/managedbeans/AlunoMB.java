7bd4397f6451f00ac8cf568689c6b1cc057df47a
acer <acer@DESKTOP-8BEQKJB>
2018-12-04 12:40:56 -0200

    Cadastro de instrutor OKAY

--- ProjetoPWEB/src/main/java/managedbeans/AlunoMB.java
+++ ProjetoPWEB/src/main/java/managedbeans/AlunoMB.java
@@ -1,12 +1,8 @@
 package managedbeans;
 
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
-
-
 import entidades.Aluno;
 import service.AlunoService;
 
--- ProjetoPWEB/src/main/java/managedbeans/InstrutorMB.java
+++ ProjetoPWEB/src/main/java/managedbeans/InstrutorMB.java
@@ -1,5 +1,35 @@
 package managedbeans;
 
-public class InstrutorMB {
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import entidades.Instrutor;
+import service.InstrutorService;
 
+@ManagedBean
+@SessionScoped
+public class InstrutorMB {
+	private Instrutor instrutor = new Instrutor();
+	private InstrutorService service = new InstrutorService();
+	
+	public void salvar() {
+		service.save(instrutor);
+		service.closeEntityManager();
+		instrutor = new Instrutor(null,null);
+	}
+	
+	public List<Instrutor> getInstrutores(){
+		List <Instrutor> lista;
+		lista = service.getAll(Instrutor.class);
+		return lista;
+	}
+
+	public Instrutor getInstrutor() {
+		return instrutor;
+	}
+
+	public void setInstrutor(Instrutor instrutor) {
+		this.instrutor = instrutor;
+	}
+	
 }
