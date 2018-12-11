58d7ead2a57ff9009e6f918243f8ed92b95b9660
delonhenrique <delon.henrique@gmail.com>
2018-12-11 14:21:55 -0200

    AlunoMB

--- ProjetoPWEB/src/main/java/managedbeans/AlunoMB.java
+++ ProjetoPWEB/src/main/java/managedbeans/AlunoMB.java
@@ -11,14 +11,8 @@ import service.AlunoService;
 public class AlunoMB {
 	private Aluno aluno = new Aluno(null,null);
 	private AlunoService service = new AlunoService();
-	private boolean flagConsultar = false;
-	private boolean flagInserir = true;
-	private boolean flagDeletar = true;
-	private boolean flagAlterar = true;
-	private boolean flagCancelar = true;
-	private boolean flagText = true;
 	
-	/*public void salvar() {
+	public void salvar() {
 		service.save(aluno);
 		service.closeEntityManager();
 		aluno = new Aluno(null,null);
@@ -32,7 +26,6 @@ public class AlunoMB {
 			System.out.println(resultado.getCpf());
 			System.out.println(resultado.getNome());
 			aluno = resultado;
-			
 			} catch (Exception  e) {
 				System.out.println("CPF não encontrado");
 			}
@@ -48,73 +41,6 @@ public class AlunoMB {
 			return null;
 		}
 		return resultado;
-	}*/
-	
-	public void consultar() {
-		List<Aluno> lista = getAlunos();
-		Aluno resultado = new Aluno(null,null);
-		try {
-			resultado = lista.stream().filter(pessoa -> pessoa.getCpf().equals(aluno.getCpf())).findFirst().get();
-			System.out.println(resultado.getCpf());
-			System.out.println(resultado.getNome());
-			} catch (Exception  e) {
-				System.out.println("CPF não encontrado");
-			}
-		
-		if(resultado == null) {
-			setFlagConsultar(true);
-			setFlagText(false);
-			setFlagInserir(false);
-			setFlagDeletar(true);
-			setFlagAlterar(true);
-			setFlagCancelar(false);
-			
-		}else {
-			aluno = resultado;
-			setFlagText(false);
-			setFlagConsultar(true);
-			setFlagInserir(true);
-			setFlagDeletar(false);
-			setFlagAlterar(false);
-			setFlagCancelar(false);
-			
-		}
-	}
-
-	public void salvar() {
-	
-		service.save(aluno);
-		service.closeEntityManager();
-		aluno = new Aluno();
-		setFlagConsultar(false);
-		setFlagInserir(true);
-		setFlagDeletar(true);
-		setFlagAlterar(true);
-		setFlagCancelar(true);
-		setFlagText(true);
-	}
-	
-	public void alterar() {
-		service.save(aluno);
-		service.closeEntityManager();
-		aluno = new Aluno();
-		setFlagConsultar(false);
-		setFlagInserir(true);
-		setFlagDeletar(true);
-		setFlagAlterar(true);
-		setFlagCancelar(true);
-		setFlagText(true);
-	}
-	
-	public void deletar() {
-		service.remove(aluno);
-		aluno = new Aluno();
-		setFlagConsultar(false);
-		setFlagInserir(true);
-		setFlagDeletar(true);
-		setFlagAlterar(true);
-		setFlagCancelar(true);
-		setFlagText(true);
 	}
 	
 	public List<Aluno> getAlunos() {
@@ -133,54 +59,6 @@ public class AlunoMB {
 
 	public void setAluno(Aluno aluno) {
 		this.aluno = aluno;
-	}
-
-	public boolean isFlagConsultar() {
-		return flagConsultar;
-	}
-
-	public void setFlagConsultar(boolean flagConsultar) {
-		this.flagConsultar = flagConsultar;
-	}
-
-	public boolean isFlagInserir() {
-		return flagInserir;
-	}
-
-	public void setFlagInserir(boolean flagInserir) {
-		this.flagInserir = flagInserir;
-	}
-
-	public boolean isFlagDeletar() {
-		return flagDeletar;
-	}
-
-	public void setFlagDeletar(boolean flagDeletar) {
-		this.flagDeletar = flagDeletar;
-	}
-
-	public boolean isFlagAlterar() {
-		return flagAlterar;
-	}
-
-	public void setFlagAlterar(boolean flagAlterar) {
-		this.flagAlterar = flagAlterar;
-	}
-
-	public boolean isFlagCancelar() {
-		return flagCancelar;
-	}
-
-	public void setFlagCancelar(boolean flagCancelar) {
-		this.flagCancelar = flagCancelar;
-	}
-
-	public boolean isFlagText() {
-		return flagText;
-	}
-
-	public void setFlagText(boolean flagText) {
-		this.flagText = flagText;
 	}
 
 	
